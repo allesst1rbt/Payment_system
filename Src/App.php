086f2b6786f6cb0f605e521ba15bbe548e3e1e97
carlos dias <carloseduardodc090@gmail.com>
2025-07-02 13:36:52 -0400

    chore: docker

--- Src/App.php
+++ Src/App.php
@@ -2,26 +2,11 @@
 
 namespace Src;
 
-use Enqueue\SimpleClient\SimpleClient;
 
 class App
 {
     public static function run()
     {
-        $client = new SimpleClient('fs://'.__DIR__.'/queue');
-        $client->setupBroker();
-        $client->bindTopic('account.created', function (\Interop\Queue\Message $message) {
-            $data = json_decode($message->getBody(), true);
-            echo 'New account created: '.$data['email'].PHP_EOL;
-        });
-
-        $client->consume();
-
-        $client->sendEvent('account.created', [
-            'id' => 1,
-            'email' => 'test@example.com',
-            'document' => '12345678901',
-        ]);
         Logger::enableSystemLogs();
     }
 }
